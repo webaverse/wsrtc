bbc45ca21f6d4c868636ec7c0ad2167864d9272e
Avaer Kazmer <a@modules.io>
2021-08-24 07:04:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 const express = require('express');
-const ws = require('ws');
+const xrrtcServer = require('./xrrtc-server.js');
 
 function getExt(fileName) {
   const match = fileName
@@ -63,56 +63,6 @@ app.get('*', (req, res, next) => {
 });
 app.use(appStatic);
 
-const wss = new ws.WebSocketServer({
-  noServer: true,
-});
-const users = [];
-wss.on('connection', (ws, req) => {
-  const id = Math.floor(Math.random() * 0xFFFFFF);
-  const localUser = {
-    id,
-    ws,
-  };
-  users.push(localUser);
-  ws.addEventListener('close', () => {
-    for (const user of users) {
-      if (user !== localUser) {
-        user.ws.send(JSON.stringify({
-          method: 'leave',
-          id,
-        }));
-      }
-    }
-    
-    users.splice(users.indexOf(localUser), 1);
-  });
-  ws.send(JSON.stringify({
-    method: 'init',
-    args: {
-      id,
-      users: users.map(u => u.id),
-    },
-  }));
-  for (const user of users) {
-    if (user !== localUser) {
-      user.ws.send(JSON.stringify({
-        method: 'join',
-        id,
-      }));
-    }
-  }
-  
-  // console.log('got ws', req.url);
-  ws.addEventListener('message', e => {
-    // console.log('got message', e.data);
-    for (const user of users) {
-      if (user !== localUser) {
-        user.ws.send(e.data);
-      }
-    }
-  });
-});
-
 const servers = [];
 const httpServer = http.createServer(app)
   .listen(httpPort);
@@ -128,9 +78,5 @@ if (CERT && PRIVKEY) {
   console.log('https://localhost:'+httpsPort);
 }
 for (const server of servers) {
-  server.on('upgrade', (req, socket, head) => {
-    wss.handleUpgrade(req, socket, head, ws => {
-      wss.emit('connection', ws, req);
-    });
-  });
+  xrrtcServer.bindServer(server);
 }
