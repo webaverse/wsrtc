ba3872c8b6167909621dae56b16e8c0691e8d96f
Avaer Kazmer <a@modules.io>
2021-08-24 20:17:53 -0400

    Update wsrtc.js

--- wsrtc.js
+++ wsrtc.js
@@ -97,7 +97,16 @@ class Player extends EventTarget {
     this.lastMessage = null;
     
     const demuxAndPlay = audioData => {
-      const channelData = getAudioDataBuffer(audioData);
+      let channelData;
+      if (audioData.copyTo) { // new api
+        channelData = new Float32Array(audioData.numberOfFrames);
+        audioData.copyTo(channelData, {
+          planeIndex: 0,
+          frameCount: audioData.numberOfFrames,
+        });
+      } else { // old api
+        channelData = audioData.buffer.getChannelData(0);
+      }
       audioWorkletNode.port.postMessage(channelData, [channelData.buffer]);
     };
     function onDecoderError(err) {
