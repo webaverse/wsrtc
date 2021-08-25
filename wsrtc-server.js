ade4fe2e90eb0ba66727e8550a025cd5832fe633
Avaer Kazmer <a@modules.io>
2021-08-25 04:21:32 -0400

    Update wsrtc-server.js

--- wsrtc-server.js
+++ wsrtc-server.js
@@ -1,4 +1,13 @@
 const ws = require('ws');
+const Y = require('yjs');
+
+const jsonParse = s => {
+  try {
+    return JSON.parse(s);
+  } catch (err) {
+    return null;
+  }
+};
 
 class User {
   constructor(id, ws) {
@@ -17,6 +26,7 @@ class Room {
   constructor(url) {
     this.url = url;
     this.users = [];
+    this.state = new Y.Doc();
   }
 }
 
@@ -45,6 +55,8 @@ wss.on('connection', (ws, req) => {
     
     room.users.splice(room.users.indexOf(localUser), 1);
   });
+  
+  // send init
   ws.send(JSON.stringify({
     method: 'init',
     args: {
@@ -52,6 +64,22 @@ wss.on('connection', (ws, req) => {
       users: room.users,
     },
   }));
+  
+  // send initial state
+  {
+    const updateBuffer = Y.encodeStateAsUpdate(room.state);
+    const b = Buffer.alloc(Uint32Array.BYTES_PER_ELEMENT + updateBuffer.byteLength);
+    new Uint32Array(b.buffer, b.byteOffset, 1)[0] = 0;
+    b.set(updateBuffer, Uint32Array.BYTES_PER_ELEMENT);
+    
+    ws.send(JSON.stringify({
+      method: 'stateupdate',
+      id: 0,
+    }));
+    ws.send(b);
+  }
+
+  // notify users about the join
   for (const user of room.users) {
     if (user !== localUser) {
       user.ws.send(JSON.stringify({
@@ -65,6 +93,26 @@ wss.on('connection', (ws, req) => {
     for (const user of room.users) {
       if (user !== localUser) {
         user.ws.send(e.data);
+      }
+    }
+    
+    if (typeof e.data === 'string') {
+      const j = jsonParse(e.data);
+      if (j) {
+        const {method} = j;
+        switch (method) {
+          case 'stateupdate': {
+            localUser.lastMessage = j;
+            break;
+          }
+        }
+      } else {
+        localUser.lastMessage = null;
+      }
+    } else {
+      if (localUser.lastMessage && localUser.lastMessage.method === 'stateupdate') {
+        Y.applyUpdate(room.state, e.data);
+        localUser.lastMessage = null;
       }
     }
   });
