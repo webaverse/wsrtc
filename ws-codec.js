caf62e980204c635700394350a616c4862f12a0b
Avaer Kazmer <a@modules.io>
2021-08-24 20:27:07 -0400

    Update ws-codec.js

--- ws-codec.js
+++ ws-codec.js
@@ -1,45 +1,6 @@
-/* const muxAndSend = encodedChunk => {
-  // console.log('got chunk', encodedChunk);
-  const {type, timestamp, duration} = encodedChunk;
-  const byteLength = encodedChunk.copyTo ?
-    encodedChunk.byteLength
-  :
-    encodedChunk.data.byteLength;
-  const data = new Uint8Array(
-    Uint32Array.BYTES_PER_ELEMENT +
-    byteLength
-  );
-  const uint32Array = new Uint32Array(data.buffer, data.byteOffset, 1);
-  uint32Array[0] = this.localUser.id;
-  if (!this.localUser.id) {
-    debugger;
-  }
-  if (encodedChunk.copyTo) { // new api
-    encodedChunk.copyTo(new Uint8Array(data.buffer, data.byteOffset + Uint32Array.BYTES_PER_ELEMENT));
-  } else { // old api
-    data.set(new Uint8Array(encodedChunk.data), Uint32Array.BYTES_PER_ELEMENT);
-  }
-  this.ws.send(JSON.stringify({
-    method: 'audio',
-    id: this.localUser.id,
-    args: {
-      type,
-      timestamp,
-      duration,
-    },
-  }));
-  this.ws.send(data);
-};
-const audioEncoder = new AudioEncoder({
-  output: muxAndSend,
-  error: onEncoderError,
-});
-audioEncoder.configure({
-  codec: 'opus',
-  numberOfChannels: channelCount,
-  sampleRate,
-  bitrate,
-}); */
+import {channelCount, sampleRate, bitrate} from './ws-constants.js';
+
+// WebCodec suport
 
 export function WsMediaStreamAudioReader(mediaStream) {
   const audioTracks = mediaStream.getAudioTracks();
@@ -49,20 +10,49 @@ export function WsMediaStreamAudioReader(mediaStream) {
   return audioReader;
 }
 
-/* export class WsAudioReader {
+export function WsAudioEncoder({output, error}) {
+  const audioEncoder = new AudioEncoder({
+    output,
+    error,
+  });
+  audioEncoder.configure({
+    codec: 'opus',
+    numberOfChannels: channelCount,
+    sampleRate,
+    bitrate,
+  });
+  return audioEncoder;
+}
+
+export function WsAudioDecoder({output, error}) {
+  const audioDecoder = new AudioDecoder({
+    output,
+    error,
+  });
+  audioDecoder.configure({
+    codec: 'opus',
+    numberOfChannels: channelCount,
+    sampleRate,
+  });
+  return audioDecoder;
+}
+
+// NO WebCodec suport
+
+/* export class WsMediaStreamAudioReader {
   contructor(mediaStream) {
 
   }
+}
+
+export class WsAudioEncoder {
+  contructor({output, error}) {
+    this.worker = new Worker('ws-codec-worker.js');
+  }
+}
+
+export class WsAudioDecoder {
+  constructor({output, error}) {
+    this.worker = new Worker('ws-codec-worker.js');
+  }
 } */
-
-export class WsOpusEncoder {
-  contructor() {
-    this.worker = new Worker('ws-codec-worker.js');
-  }
-}
-
-export class WsOpusDecoder {
-  constructor() {
-    this.worker = new Worker('ws-codec-worker.js');
-  }
-}
