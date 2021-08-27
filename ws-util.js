ad3aa70d29c824268bf66813a58ef457c83c047f
Avaer Kazmer <a@modules.io>
2021-08-26 21:41:05 -0400

    Update ws-util.js

--- ws-util.js
+++ ws-util.js
@@ -2,45 +2,31 @@ import {TYPE} from './ws-constants.js';
 
 const textEncoder = new TextEncoder();
 const textDecoder = new TextDecoder();
+const encodedMessageUint8Array = new Uint8Array(4096);
+const encodedMessageUint32Array = new Uint32Array(encodedMessageUint8Array.buffer);
 export const encodeMessage = parts => {
-  let byteLength = 0;
-  parts = parts.map(part => {
-    if (typeof part === 'number') {
-      byteLength += Uint32Array.BYTES_PER_ELEMENT;
-      return part;
-    } else if (typeof part === 'string') {
-      const b = textEncoder.encode(part);
-      byteLength += Uint32Array.BYTES_PER_ELEMENT;
-      byteLength += b.byteLength;
-      return b;
-    } else if (typeof part.byteLength === 'number') {
-      if (!part.staticSize) {
-        byteLength += Uint32Array.BYTES_PER_ELEMENT;
-      }
-      byteLength += part.byteLength;
-      return part;
-    } else {
-      throw new Error('unknown part: ' + JSON.stringify(part));
-    }
-  });
-
-  const data = new Uint8Array(byteLength);
-  const uint32Array = new Uint32Array(data.buffer, data.byteOffset, Math.floor(byteLength/Uint32Array.BYTES_PER_ELEMENT));
   let index = 0;
   for (const part of parts) {
     if (typeof part === 'number') {
-      uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = part;
+      encodedMessageUint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = part;
       index += Uint32Array.BYTES_PER_ELEMENT;
-    } else {
+    } else if (typeof part === 'string') {
+      const {written} = textEncoder.encodeInto(part, new Uint8Array(encodedMessageUint8Array.buffer, encodedMessageUint8Array.byteOffset + index + Uint32Array.BYTES_PER_ELEMENT));
+      encodedMessageUint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = written;
+      index += Uint32Array.BYTES_PER_ELEMENT;
+      index += written;
+    } else if (part.byteLength >= 0) {
       if (!part.staticSize) {
-        uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = part.byteLength;
+        encodedMessageUint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = part;
         index += Uint32Array.BYTES_PER_ELEMENT;
       }
-      data.set(new Uint8Array(part.buffer, part.byteOffset, part.byteLength), index);
+      encodedMessageUint8Array.set(new Uint8Array(part.buffer, part.byteOffset, part.byteLength), index);
       index += part.byteLength;
+    } else {
+      throw new Error('unknown part: ' + JSON.stringify(part));
     }
   }
-  return data;
+  return new Uint8Array(encodedMessageUint8Array.buffer, encodedMessageUint8Array.byteOffset, index);
 };
 const _align = (index, n) => index + (n - (index % n));
 const _align4 = index => _align(index, 4);
