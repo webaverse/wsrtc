09856de711a06ed6b65ea20f15e91da7f2a9b256
Avaer Kazmer <a@modules.io>
2021-08-26 22:18:21 -0400

    Update ws-util.js

--- ws-util.js
+++ ws-util.js
@@ -3,21 +3,21 @@ import {TYPE} from './ws-constants.js';
 const textEncoder = new TextEncoder();
 const textDecoder = new TextDecoder();
 const encodedMessageUint8Array = new Uint8Array(4096);
-const encodedMessageUint32Array = new Uint32Array(encodedMessageUint8Array.buffer);
+const encodedMessageDataView = new DataView(encodedMessageUint8Array.buffer);
 export const encodeMessage = parts => {
   let index = 0;
   for (const part of parts) {
     if (typeof part === 'number') {
-      encodedMessageUint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = part;
+      encodedMessageDataView.setUint32(index, part, true);
       index += Uint32Array.BYTES_PER_ELEMENT;
     } else if (typeof part === 'string') {
       const {written} = textEncoder.encodeInto(part, new Uint8Array(encodedMessageUint8Array.buffer, encodedMessageUint8Array.byteOffset + index + Uint32Array.BYTES_PER_ELEMENT));
-      encodedMessageUint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = written;
+      encodedMessageDataView.setUint32(index, written, true);
       index += Uint32Array.BYTES_PER_ELEMENT;
       index += written;
     } else if (part.byteLength >= 0) {
       if (!part.staticSize) {
-        encodedMessageUint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = part.byteLength;
+        encodedMessageDataView.setUint32(index, part.byteLength, true);
         index += Uint32Array.BYTES_PER_ELEMENT;
       }
       encodedMessageUint8Array.set(new Uint8Array(part.buffer, part.byteOffset, part.byteLength), index);
