cf21115f7befcb723dd131202d3fe7cf991bc222
Avaer Kazmer <a@modules.io>
2021-08-26 21:04:30 -0400

    Update ws-util.js

--- ws-util.js
+++ ws-util.js
@@ -1,4 +1,7 @@
+import {TYPE} from './ws-constants.js';
+
 const textEncoder = new TextEncoder();
+const textDecoder = new TextDecoder();
 export const encodeMessage = parts => {
   let byteLength = 0;
   parts = parts.map(part => {
@@ -38,6 +41,135 @@ export const encodeMessage = parts => {
     }
   }
   return data;
+};
+const _align = (index, n) => index + (n - (index % n));
+const _align4 = index => _align(index, 4);
+export const encodeTypedMessage = (uint8Array, parts) => {
+  const uint32Array = new Uint32Array(uint8Array.buffer, uint8Array.byteOffset, uint8Array.byteLength/Uint32Array.BYTES_PER_ELEMENT);
+  const float32Array = new Float32Array(uint8Array.buffer, uint8Array.byteOffset, uint8Array.byteLength/Float32Array.BYTES_PER_ELEMENT);
+  
+  let index = 0;
+  for (const part of parts) {
+    if (typeof part === 'number') {
+      if (Number.isInteger(part)) {
+        uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = TYPE.INT;
+        index += Uint32Array.BYTES_PER_ELEMENT;
+        uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = part;
+        index += Uint32Array.BYTES_PER_ELEMENT;
+      } else {
+        uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = TYPE.FLOAT;
+        index += Uint32Array.BYTES_PER_ELEMENT;
+        float32Array[index/Float32Array.BYTES_PER_ELEMENT] = part;
+        index += Uint32Array.BYTES_PER_ELEMENT;
+      }
+    } else if (typeof part === 'string') {
+      uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = TYPE.STRING;
+      index += Uint32Array.BYTES_PER_ELEMENT;
+      const b = textEncoder.encode(part);
+      uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = b.byteLength;
+      index += Uint32Array.BYTES_PER_ELEMENT;
+      
+      uint8Array.set(b, index);
+      index += b.byteLength;
+      index = _align4(index);
+    } else if (part instanceof Uint32Array) {
+      uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = TYPE.UINT32ARRAY;
+      index += Uint32Array.BYTES_PER_ELEMENT;
+      uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = part.length;
+      index += Uint32Array.BYTES_PER_ELEMENT;
+      
+      uint32Array.set(part, index/Uint32Array.BYTES_PER_ELEMENT);
+      index += part.byteLength;
+    } else if (part instanceof Float32Array) {
+      uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = TYPE.FLOAT32ARRAY;
+      index += Uint32Array.BYTES_PER_ELEMENT;
+      uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = part.length;
+      index += Uint32Array.BYTES_PER_ELEMENT;
+      
+      float32Array.set(part, index/Float32Array.BYTES_PER_ELEMENT);
+      index += part.byteLength;
+    } else if (part instanceof Uint8Array) {
+      uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = TYPE.UINT32ARRAY;
+      index += Uint32Array.BYTES_PER_ELEMENT;
+      uint32Array[index/Uint32Array.BYTES_PER_ELEMENT] = part.length;
+      index += Uint32Array.BYTES_PER_ELEMENT;
+      
+      uint8Array.set(part, index);
+      index += part.byteLength;
+      index = _align4(index);
+    } else {
+      throw new Error('unknown part: ' + JSON.stringify(part));
+    }
+  }
+  return index;
+};
+export const decodeTypedMessage = (uint8Array, uint8ArrayByteLength, parts) => {
+  const uint32Array = new Uint32Array(uint8Array.buffer, uint8Array.byteOffset, uint8ArrayByteLength/Uint32Array.BYTES_PER_ELEMENT);
+  const float32Array = new Float32Array(uint8Array.buffer, uint8Array.byteOffset, uint8ArrayByteLength/Float32Array.BYTES_PER_ELEMENT);
+  
+  parts.length = 0;
+  for (let index = 0; index < uint8ArrayByteLength;) {
+    const type = uint32Array[index/Uint32Array.BYTES_PER_ELEMENT];
+    index += Uint32Array.BYTES_PER_ELEMENT;
+
+    switch (type) {
+      case TYPE.INT: {
+        const part = uint32Array[index/Uint32Array.BYTES_PER_ELEMENT];
+        parts.push(part);
+        index += Uint32Array.BYTES_PER_ELEMENT;
+        break;
+      }
+      case TYPE.FLOAT: {
+        const part = float32Array[index/Float32Array.BYTES_PER_ELEMENT];
+        parts.push(part);
+        index += Float32Array.BYTES_PER_ELEMENT;
+        break;
+      }
+      case TYPE.STRING: {
+        const byteLength = uint32Array[index/Uint32Array.BYTES_PER_ELEMENT];
+        index += Uint32Array.BYTES_PER_ELEMENT;
+        
+        const b = new Uint8Array(uint8Array.buffer, index, byteLength);
+        const part = textDecoder.decode(b);
+        parts.push(part);
+        index += byteLength;
+        index = _align4(index);
+        break;
+      }
+      case TYPE.UINT32ARRAY: {
+        const length = uint32Array[index/Uint32Array.BYTES_PER_ELEMENT];
+        index += Uint32Array.BYTES_PER_ELEMENT;
+        
+        const part = new Uint32Array(uint8Array.buffer, index, length);
+        parts.push(part);
+        index += part.byteLength;
+        break;
+      }
+      case TYPE.FLOAT32ARRAY: {
+        const length = uint32Array[index/Uint32Array.BYTES_PER_ELEMENT];
+        index += Uint32Array.BYTES_PER_ELEMENT;
+        
+        const part = new Float32Array(uint8Array.buffer, index, length);
+        parts.push(part);
+        index += part.byteLength;
+        break;
+      }
+      case TYPE.UINT8ARRAY: {
+        const length = uint32Array[index/Uint32Array.BYTES_PER_ELEMENT];
+        index += Uint32Array.BYTES_PER_ELEMENT;
+        
+        const part = new Uint8Array(uint8Array.buffer, index, length);
+        parts.push(part);
+        index += part.byteLength;
+        index = _align4(index);
+        break;
+      }
+      default: {
+        throw new Error('cannot parse message part with type ' + type);
+        break;
+      }
+    }
+  }
 };
 export const getAudioDataBuffer = audioData => {
   let channelData;
