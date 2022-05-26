faa7f1017493788049ea0e68953c0f6627aa4c74
tcm390 <60634884+tcm390@users.noreply.github.com>
2022-05-26 12:43:20 -0400

    load avatar.audioContext

--- ws-audio-context.js
+++ ws-audio-context.js
@@ -1,12 +1,10 @@
 import {channelCount, sampleRate, bitrate} from './ws-constants.js';
-
+import Avatar from '../../avatars/avatars.js';
 let audioCtx = null;
 export const ensureAudioContext = async () => {
-  if (!audioCtx) {
-    audioCtx = new AudioContext({
-      latencyHint: 'interactive',
-      sampleRate,
-    });
+  if (!audioCtx && Avatar.audioContext) {
+    
+    audioCtx = Avatar.audioContext;
     await Promise.all([
       audioCtx.audioWorklet.addModule(`${import.meta.url.replace(/(\/)[^\/]*$/, '$1')}ws-input-worklet.js`),
       audioCtx.audioWorklet.addModule(`${import.meta.url.replace(/(\/)[^\/]*$/, '$1')}ws-output-worklet.js`),
