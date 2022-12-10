627817c420000fc3aa370a4179ca3322a8404d50
Avaer Kazmer <a@modules.io>
2022-12-10 17:22:58 -0500

    Small cleanup

--- ws-audio-context.js
+++ ws-audio-context.js
@@ -1,15 +1,20 @@
-import {channelCount, sampleRate, bitrate} from './ws-constants.js';
+import {/* channelCount, */sampleRate/*, bitrate*/} from './ws-constants.js';
+
+//
 
 let audioCtx = null;
 export const ensureAudioContext = async () => {
-  if (!audioCtx) {
+  if (!audioCtx && typeof AudioContext !== 'undefined') {
     audioCtx = new AudioContext({
       latencyHint: 'interactive',
       sampleRate,
     });
+
+    const inputWorkletUrl = new URL(`./ws-input-worklet.js`, import.meta.url);
+    const outputWorkletUrl = new URL(`./ws-output-worklet.js`, import.meta.url);
     await Promise.all([
-      audioCtx.audioWorklet.addModule(`${import.meta.url.replace(/(\/)[^\/]*$/, '$1')}ws-input-worklet.js`),
-      audioCtx.audioWorklet.addModule(`${import.meta.url.replace(/(\/)[^\/]*$/, '$1')}ws-output-worklet.js`),
+      audioCtx.audioWorklet.addModule(inputWorkletUrl),
+      audioCtx.audioWorklet.addModule(outputWorkletUrl),
     ]);
   }
 };
